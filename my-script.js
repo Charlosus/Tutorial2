4a28723f6cbca68fa2dc3061d1fd6448f38afe82
Charlosus <145377783+Charlosus@users.noreply.github.com>
2025-05-05 06:46:20 +0200

    Alot

---
+++ my-script.js
@@ -0,0 +1,38 @@
+// const a = "Hello, world!";
+// console.log(a);
+
+// // adding number wariables
+// const b = 5;
+// const c = 10;   
+// const sum = b + c;
+// console.log(sum);
+
+// // add if 
+// var x = 10;
+// if (x > 5) {
+//     console.log("x is greater than 5");
+
+// console API
+
+// console.log("Hello there")
+
+// const message = "General kenobi";
+
+// console.time("timerLabel")
+
+// console.warn(message)
+// console.error(message)
+
+// console.timeLog("timerLabel")
+
+// console.table(["Adam", "Beth", "Cecil" ]);
+
+// console.log("https://google.pl")
+
+// console.timeEnd("timerLabel")
+
+// console.clear()
+
+const x =  5 + 5
+console.log( x)
+console.log( {x})
